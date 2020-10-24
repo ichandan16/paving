31f82c772875626060eee3bdbd51694a3251ae60
206464754 <a206464754@macbook.tfayd.com>
2020-10-23 22:44:07 -0400

    deleted other pavings except gcp

--- aws/pks-route-tables.tf
+++
@@ -1,5 +0,0 @@
-resource "aws_route_table_association" "route-pks-subnet" {
-  count          = length(var.availability_zones)
-  subnet_id      = element(aws_subnet.pks-subnet[*].id, count.index)
-  route_table_id = element(aws_route_table.deployment[*].id, count.index)
-}
